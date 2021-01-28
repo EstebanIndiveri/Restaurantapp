ac69faf5167b19624ae1615230df6866e12fe97f
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-01-27 22:05:49 -0300

    test enviroment

---
+++ src/components/ui/Test.jsx
@@ -0,0 +1,11 @@
+import React,{Fragment} from 'react';
+
+const Test = () => {
+    return ( 
+        <Fragment>
+            <Text>Test enviroment dot</Text>
+        </Fragment>
+     );
+}
+ 
+export default Test;

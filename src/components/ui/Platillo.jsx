67b4f38ed21eba61db79f6021335a08944ce026a
Esteban Indiveri <esteban.indiveri@gmail.com>
2021-01-31 21:10:07 -0300

    update useRef available product

--- src/components/ui/Platillo.jsx
+++ src/components/ui/Platillo.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-
+import React,{useContext,useRef} from 'react';
+import{FirebaseContext} from '../../firebase';
 
 const Platillo = ({platillo}) => {
-    const{nombre,imagen}=platillo
+    // ref valor del dom
+    const existenciaRef=useRef(platillo.existencia);
+    const{id,nombre,imagen,existencia,categoria,precio,descripcion}=platillo
+
+    // 
+    const{firebase}=useContext(FirebaseContext);
+
+    const actualizarDisponibilidad=()=>{
+        const existencia=(existenciaRef.current.value==='true');
+        console.log(existencia);
+        try {
+            firebase.db.collection('productos').doc(id).update({
+                existencia
+            });
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }
+
     return ( 
         <div className="w-full px-3 mb-4">
 
@@ -10,9 +29,30 @@ const Platillo = ({platillo}) => {
                 <div className="lg:flex">
                     <div className="lg:w-5/12 xl:w-3/12">
                         <img src={imagen} alt="imagen platillo"/>
+                        <div className="sm:flex sm:-xm-2 pl-2">
+                            <label className="block mt-5 sm:w-2/4">
+                                <span className="block text-gray-800 mb-2">Existencia</span>
+                                <select 
+                                className="bg-white shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline transition-all  duration-300 ease-in-out "
+                                value={existencia}
+                                ref={existenciaRef}
+                                onChange={()=>actualizarDisponibilidad()}
+                                >
+                                    <option value="true">Disponible</option>
+                                    <option value="false">No Disponible</option>
+
+                                </select>
+                            </label>
+                        </div>
                     </div>
-                    <div className="lg:w-7/12 xl:w-9/12">
-                        2
+                    <div className="lg:w-7/12 xl:w-9/12 pl-5
+                    ">
+                        <p className="font-bold text-2xl text-yellow-600 mb-4">{nombre}</p>
+                        <p className="text-gray-600 mb-4">Categoría: {' '}
+                        <span className="text-gray-700 font-bold">{categoria.toUpperCase()}</span></p>
+                        <p className="text-gray-600 mb-4">{descripcion}</p>
+                        <p className="text-gray-600 mb-4">Precio: {' '}
+                        <span className="text-gray-700 font-bold">$ {precio}</span></p>
                     </div>
                 </div>
             </div>           
